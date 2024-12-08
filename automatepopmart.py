35407576e2b3efcf28b680e0498348341c281bc6
hleow97 <hanlileow@gmail.com>
2024-12-08 15:44:38 +0800

    fix: able to login and autocheckout

--- automatepopmart.py
+++ automatepopmart.py
@@ -1,6 +1,7 @@
 import random
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -14,7 +15,10 @@ urls = [
 ]
 
 def initialize_driver():
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(options=chrome_options)
     return driver
 
 def accept_terms(driver):
@@ -25,13 +29,42 @@ def accept_terms(driver):
     else:
         print('no T&C')
 
+def login(driver):
+    try:
+        # Click on Sign in
+        login = driver.find_element(By.XPATH, "//*[contains(text(), 'Sign in')]")
+        login.click()
+        # Wait for the login form to appear
+        WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'SIGN IN OR REGISTER')]")))
+        # Fill in the email
+        email = driver.find_element(By.XPATH, '//*[@id="email"]')
+        email.send_keys('#REDACTED')
+        # Click on continue
+        continueBtn = driver.find_element(By.XPATH, "//*[contains(text(), 'CONTINUE')]")
+        continueBtn.click()
+        # Wait for the password field to appear
+        WebDriverWait(driver, 200).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="password"]')))
+        password = driver.find_element(By.XPATH, '//*[@id="password"]')
+        password.send_keys('#REDACTED')
+        # Click on Sign in
+        signInBtn = driver.find_element(By.XPATH,'//button[normalize-space()="SIGN IN"]')
+        signInBtn.click()
+        time.sleep(10)
+        selected_url = random.choice(urls)
+        driver.get(selected_url)
+
+    except Exception as error:
+        print('login failed')
+        print(error)
+        exit()
+
 def play_sound():
     mixer.init()
     mixer.music.load("bell.wav")
-    mixer.music.play(60)
+    mixer.music.play(100)
     print("🎉 🎉 🎉")
     while mixer.music.get_busy():
-        time.sleep(120)
+        time.sleep(300)
 
 def check_availability(driver):
     try:
@@ -40,24 +73,34 @@ def check_availability(driver):
     except:
         return False
 
+def buy_now(driver):
+    try:
+        buy_now = driver.find_element(By.XPATH, "//*[contains(text(), 'BUY NOW')]")
+        buy_now.click()
+        WebDriverWait(driver, 200).until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'PROCEED TO PAY')]")))
+        time.sleep(5)
+        proceed_to_pay = driver.find_element(By.XPATH, "//*[contains(text(), 'PROCEED TO PAY')]")
+        proceed_to_pay.click()
+        play_sound()
+    except Exception as error:
+        print(error)
+        print('Buy now failed')
+        exit()
+
+
 def main():
     driver = initialize_driver()
     selected_url = random.choice(urls)
     driver.get(selected_url)
 
     accept_terms(driver)
+    login(driver)
 
     WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'Details')]")))
 
-    try:
-        driver.find_element(By.XPATH, "//*[contains(text(), 'NOTIFY ME WHEN AVAILABLE')]")
-    except:
-        play_sound()
-        exit()
-
     found = False
     while not found:
-        time.sleep(random.randrange(20,40))
+        time.sleep(random.randrange(10,20))
         selected_url = random.choice(urls)
         driver.get(selected_url)
         WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'Details')]")))
@@ -67,7 +110,7 @@ def main():
         else:
             print("Not available")
 
-    play_sound()
+    buy_now(driver)
 
 if __name__ == "__main__":
     main()
